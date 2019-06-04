4f8ecbd895c886baadf08ddf3c42cf92789b8e3b
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-06-03 23:41:45 -0300

    Created NewItem component

---
+++ src/components/TodoList/NewItem/NewItem.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import TextField from "../../TextField";
+import Button from "../../Button";
+
+export interface Props {
+  onItemCreate: Function;
+}
+
+export default function NewItem({ onItemCreate }: Props): JSX.Element {
+  const [todo, setTodo]: [string, Function] = useState("");
+  console.log(todo);
+
+  function handleTextChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setTodo(event.target.value);
+  }
+
+  function submitTodo() {
+    if (todo) {
+      onItemCreate(todo);
+      setTodo("");
+    }
+  }
+
+  return (
+    <form>
+      <TextField value={todo} onChange={handleTextChange} />
+      <Button onClick={submitTodo} variant={todo ? "primary" : "disabled"}>
+        Add
+      </Button>
+    </form>
+  );
+}
---
+++ src/components/TodoList/NewItem/index.js
@@ -0,0 +1,2 @@
+export { default } from "./NewItem";
+export * from "./NewItem";
