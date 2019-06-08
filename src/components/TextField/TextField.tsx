55af7e1c77f13540275971925f65d596732de45e
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-06-08 17:32:19 -0300

    Altered TextField to tachyons + styled-components

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import styles from "./TextField.module.css";
+import styled from "styled-components";
 
 export interface Props {
+  variant?: "primary" | "disabled";
   value: string;
+  autofocus?: boolean;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function(props: Props) {
-  return <input type="text" {...props} />;
-}
+const variants = {
+  primary: "input-reset dim ba b--black-20 pa1 helvetica",
+  disabled: "input-reset ba b--black-10 pa1 helvetica"
+};
+
+const TextField = styled.input.attrs((props: Props) => {
+  const { variant = "primary", autofocus = true, ...rest } = props;
+
+  return {
+    type: "text",
+    className: variants[variant],
+    autofocus,
+    ...rest
+  };
+})<Props>``;
+
+export default TextField;
