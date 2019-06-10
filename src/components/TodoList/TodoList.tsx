2336255215c46fc719768ca59fba5263d3115805
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-06-09 22:33:17 -0300

    Refactored TodoList

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import TodoItem from "./TodoItem";
 import NewItem from "./NewItem";
 
+const tachyons = {
+  element:
+    "flex flex-column justify-end ph4 pv1 bg-washed-red ba b--dark-pink bw1 br1"
+};
+
 export default function TodoList(): JSX.Element {
   const [list, setList]: [string[], Function] = useState([]);
 
-  function addNewItem(item: string) {
+  function addItem(item: string) {
     setList([...list, item]);
   }
 
@@ -16,15 +21,17 @@ export default function TodoList(): JSX.Element {
   }
 
   return (
-    <ul>
-      {list.map((item, index) => (
-        <TodoItem
-          key={Math.random()}
-          item={item}
-          onSelect={() => removeItem(index)}
-        />
-      ))}
-      <NewItem onItemCreate={addNewItem} />
-    </ul>
+    <div className={tachyons.element}>
+      <ul className="list pl0">
+        {list.map((item, index) => (
+          <TodoItem
+            key={Math.random()}
+            item={item}
+            onSelect={() => removeItem(index)}
+          />
+        ))}
+        <NewItem onItemCreate={addItem} />
+      </ul>
+    </div>
   );
 }
