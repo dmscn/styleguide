2313de4e866b1222ad7d2917539bed179f655fa9
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-06-09 22:27:11 -0300

    Added tachyons to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
-  return <TodoList />;
+  return (
+    <div className="flex vh-100 justify-center items-center bg-washed-yellow">
+      <TodoList />
+    </div>
+  );
 };
 
 export default App;
